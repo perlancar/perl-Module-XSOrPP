2e510105fa6ab2b42cdaa9a010e194d9f89d83a8
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-04-05 08:45:45 +0700

    Initial commit (no impl yet)

---
+++ lib/SHARYANTO/Module/Util.pm
@@ -0,0 +1,30 @@
+package SHARYANTO::Module::Util;
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+               );
+
+# VERSION
+
+1;
+# ABSTRACT: Module-related utilities
+
+=head1 SYNOPSIS
+
+ use SHARYANTO::Module::Util qw(
+ );
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<SHARYANTO>
+
+=cut
